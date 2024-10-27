44f4d8872f976eba1aa676bb1b3aa5c74a9f358a
Gilbert Carrasco <tonytrox@gmail.com>
2024-10-27 03:49:44 -0500

    Fix cardDeleteButton

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,15 @@ function Card({ name, link, likes, onCardClick, card }) {
   const currentUser = useContext(CurrentUserContext);
 
   // Verificando si el usuario actual es el propietario de la tarjeta actual
-  // const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner._id === currentUser._id;
 
   // Creando una variable que después establecerás en `className` para el botón eliminar
-  // const cardDeleteButtonClassName = `card__delete-button ${
-  //   isOwn ? "card__delete-button_visible" : "card__delete-button_hidden"
-  // }`;
+  const cardDeleteButtonClassName = `element__delete-button ${
+    isOwn ? "element__delete-button_visible" : ""
+  }`;
 
   // Verifica si el usuario actual le dio "like" a la tarjeta
-  // const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
 
   // Crea una variable que después establecerás en `className` para el botón like
   // const cardLikeButtonClassName = `...`;
@@ -32,7 +32,7 @@ function Card({ name, link, likes, onCardClick, card }) {
             <button className="element__like-button"></button>
             <p className="element__counter">{likes.length}</p>
           </div>
-          <button className="element__remove-button"></button>
+          <button className={cardDeleteButtonClassName}></button>
         </div>
       </div>
     </li>
