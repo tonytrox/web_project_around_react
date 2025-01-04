7a490170709fd772e90d7540d120ccfccecb36ba
Gilbert Carrasco <tonytrox@gmail.com>
2025-01-03 23:33:04 -0500

    Step 2: Transfer HTML markup to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from "./images/logo_aroundus.svg";
+import avatar from "./images/avatar_jacques.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <img
+          className="header__logo"
+          src={logo}
+          alt="Around the U.S logo"
+        />
+      </header>
+      <main className="content">
+        <section className="profile">
+          <img
+            className="profile__image"
+            src={avatar}
+            alt="profile avatar"
+          />
+          <div className="profile__info">
+            <h1 className="profile__title">Jacques Cousteau</h1>
+            <button className="profile__edit-button"></button>
+            <h2 className="profile__description">Explorer</h2>
+          </div>
+          <button className="profile__add-button"></button>
+        </section>
+        <section className="elements">
+          <ul className="elements__list"></ul>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer__copyright">&copy; 2023 Around The U.S.</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
